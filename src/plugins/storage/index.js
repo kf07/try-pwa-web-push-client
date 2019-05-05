cc4d96fd470b22cd017dec009bb7eb421ad61ee0
kango furusu <furusu0715@gmail.com>
2019-05-05 19:36:11 +0900

    localforage

---
+++ src/plugins/storage/index.js
@@ -0,0 +1,18 @@
+import localForage from "localforage";
+import { Storage } from "./Storage";
+
+const install = (Vue, { name = "app-storage" } = {}) => {
+  const db = localForage.createInstance({
+    name
+  });
+  const storage = new Storage({ db });
+  Object.defineProperty(Vue.prototype, "$_storage", {
+    get() {
+      return storage;
+    }
+  });
+};
+
+export default {
+  install
+};
