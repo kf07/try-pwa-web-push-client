0df8ca7fa2ef7438846c20efc59df8e7af170dd7
kango furusu <furusu0715@gmail.com>
2019-05-05 16:47:07 +0900

    axiosをプラグイン化

---
+++ src/plugins/axios/index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const install = (Vue, config = {}) => {
+  const client = axios.create(config);
+  Object.defineProperty(Vue.prototype, "$_axios", {
+    get() {
+      return client;
+    }
+  });
+};
+
+export default {
+  install
+};
